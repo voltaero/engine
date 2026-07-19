22b53fff401e197e5daab1bd9e8de30f0c7c0d68
Styly <claudiotorresptpt+git@gmail.com>
2026-07-19 19:52:26 +0100

    renew lease optmize

--- enginelib/src/nucleus/renew.rs
+++ enginelib/src/nucleus/renew.rs
@@ -2,19 +2,22 @@ use std::sync::Arc;
 
 use chrono::{DateTime, Utc};
 
-use crate::{Identifier, api::ServerAPI};
+use crate::{Identifier, api::ServerAPI, error::Error};
 
 #[allow(dead_code)]
-fn renew(api: Arc<ServerAPI>, task_type: Identifier, task_id: &str) {
-    api.leased_tasks
+fn renew(api: Arc<ServerAPI>, task_type: Identifier, task_id: &str) -> Result<(), Error> {
+    let mut leases = api
+        .leased_tasks
         .tasks
-        .entry(task_type)
-        .and_modify(|leases| {
-            if let Some(lease) = leases
-                .iter_mut()
-                .find(|lease| lease.stored_task.task_id == task_id)
-            {
-                lease.given_at = Utc::now();
-            }
-        });
+        .get_mut(&task_type)
+        .ok_or(Error::new("TaskTypeNotFound".into()))?;
+
+    let lease = leases
+        .iter_mut()
+        .find(|lease| lease.stored_task.task_id == task_id)
+        .ok_or(Error::new("TaskNotFound".into()))?;
+
+    lease.given_at = Utc::now();
+
+    Ok(())
 }
