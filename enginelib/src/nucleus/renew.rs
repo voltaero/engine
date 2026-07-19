dc38b7518f6189c1b5d2f9338e152bf41919e227
Styly <claudiotorresptpt+git@gmail.com>
2026-07-19 19:49:02 +0100

    renew lease

--- enginelib/src/nucleus/renew.rs
+++ enginelib/src/nucleus/renew.rs
@@ -1,6 +1,20 @@
 use std::sync::Arc;
 
-use crate::api::ServerAPI;
+use chrono::{DateTime, Utc};
+
+use crate::{Identifier, api::ServerAPI};
 
 #[allow(dead_code)]
-fn renew(_api: Arc<ServerAPI>) {}
+fn renew(api: Arc<ServerAPI>, task_type: Identifier, task_id: &str) {
+    api.leased_tasks
+        .tasks
+        .entry(task_type)
+        .and_modify(|leases| {
+            if let Some(lease) = leases
+                .iter_mut()
+                .find(|lease| lease.stored_task.task_id == task_id)
+            {
+                lease.given_at = Utc::now();
+            }
+        });
+}
