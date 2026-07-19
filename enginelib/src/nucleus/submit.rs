3fb1f96e6ba78fcb856a530833911e5c38618f45
Styly <claudiotorresptpt+git@gmail.com>
2026-07-19 15:28:08 +0100

    fix incorrect name

--- enginelib/src/nucleus/submit.rs
+++ enginelib/src/nucleus/submit.rs
@@ -4,19 +4,19 @@ use crate::{Identifier, Registry, api::ServerAPI, error::Error};
 // t:namespace:task_name:<id> -> Serialized Task Record
 // f:namespace:task_name:<id> -> Finished Task Record
 #[allow(dead_code)]
-pub fn submit(api: Arc<ServerAPI>, task_bytes: &[u8], task_id: Identifier) -> Result<(), Error> {
+pub fn submit(api: Arc<ServerAPI>, task_bytes: &[u8], task_type: Identifier) -> Result<(), Error> {
     // deserialize
 
     let task = api
         .task_registry
-        .get(&task_id)
+        .get(&task_type)
         .ok_or(Error::new("Not found".into()))?;
     if task.verify(task_bytes) {
         let res = api.db.put(
             format!(
                 "t:{}:{}:{}",
-                task_id.0,
-                task_id.1,
+                task_type.0,
+                task_type.1,
                 druid::Druid::default().to_hex()
             ),
             task_bytes,
