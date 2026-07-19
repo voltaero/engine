e37f2243f72f032ac6c002b336c1efbb5d5016b1
Styly <claudiotorresptpt+git@gmail.com>
2026-07-19 15:52:23 +0100

    cancel

--- enginelib/src/nucleus/cancel.rs
+++ enginelib/src/nucleus/cancel.rs
@@ -1,6 +1,14 @@
 use std::sync::Arc;
 
-use crate::api::ServerAPI;
+use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 
-#[allow(dead_code)]
-fn cancel(_api: Arc<ServerAPI>) {}
+use crate::{Identifier, api::ServerAPI, error::Error};
+
+pub fn cancel(api: Arc<ServerAPI>, task_type: Identifier, task_id: String) -> Result<(), Error> {
+    api.leased_tasks.tasks.entry(task_type).and_modify(|f| {
+        f.par_iter()
+            .filter(|f| f.stored_task.task_id != task_id)
+            .collect::<Vec<_>>();
+    });
+    Ok(())
+}
