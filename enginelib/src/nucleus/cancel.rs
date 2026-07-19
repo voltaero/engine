e434fa011b51775646337749b184e26bad9db40b
Styly <claudiotorresptpt+git@gmail.com>
2026-07-19 19:36:06 +0100

    fix state cancel lease

--- enginelib/src/nucleus/cancel.rs
+++ enginelib/src/nucleus/cancel.rs
@@ -5,10 +5,16 @@ use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 use crate::{Identifier, api::ServerAPI, error::Error};
 
 pub fn cancel(api: Arc<ServerAPI>, task_type: Identifier, task_id: String) -> Result<(), Error> {
-    api.leased_tasks.tasks.entry(task_type).and_modify(|f| {
-        f.par_iter()
-            .filter(|f| f.stored_task.task_id != task_id)
-            .collect::<Vec<_>>();
+    api.leased_tasks
+        .tasks
+        .entry(task_type.clone())
+        .and_modify(|f| {
+            f.par_iter()
+                .filter(|f| f.stored_task.task_id != task_id)
+                .collect::<Vec<_>>();
+        });
+    api.task_queue.tasks.entry(task_type).and_modify(|f| {
+        f.2.remove(&task_id);
     });
     Ok(())
 }
